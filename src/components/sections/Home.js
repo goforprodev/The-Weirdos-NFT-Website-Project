7609451633e35c4a749effbddb804e7a9a8e192c
goforpordev <goforprodev@gmail.com>
2022-08-27 11:31:28 +0100

    Added Spinner

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import CoverVideo from "../CoverVideo";
 import TypewriterText from "../TypewriterText";
+import RoundTextBlack from "../../assets/Rounded-Text-Black.png";
 
 const Section = styled.section`
   min-height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
@@ -28,6 +29,46 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const rotate = keyframes`
+100%{
+  transform:rotate(1turn)
+
+}
+
+`;
+const Round = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  right: 90%;
+  width: 6rem;
+  height: 6rem;
+  border: 1px solid ${(props) => props.theme.text};
+  border-radius:50%;
+  
+  img {
+    width: 100%;
+    height: auto;
+    animation: ${rotate} 6s linear infinite reverse;
+  }
+`;
+
+const Circle = styled.span`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: ${(props) => props.theme.fontxl};
+`;
+
 const Home = () => {
   return (
     <Section>
@@ -38,6 +79,10 @@ const Home = () => {
         <Box>
           <CoverVideo />
         </Box>
+        <Round>
+          <Circle>&#x2193;</Circle>
+          <img src={RoundTextBlack} alt={"NFT"} />
+        </Round>
       </Container>
     </Section>
   );
